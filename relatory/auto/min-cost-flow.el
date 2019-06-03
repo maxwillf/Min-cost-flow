a61b1f3974ff832f7791688fb6071fa71d8f51ed
João Pedro <jpedrodeamorim@gmail.com>
2019-06-03 20:34:09 -0300

    Added Dijkstra to relatory and some more ignores

--- relatory/auto/min-cost-flow.el
+++ relatory/auto/min-cost-flow.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "english")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -24,8 +24,20 @@
     "babel"
     "biblatex")
    (LaTeX-add-labels
-    "fig:graph"
-    "fig:alg")
+    "sec:intro"
+    "sec:modelling"
+    "fig:modeled-graph"
+    "subsec:vertex-structure"
+    "subsec:arches"
+    "sec:state-of-the-art"
+    "subsec:min-mean-cycle-cancelling"
+    "subsec:network-simplex"
+    "fig:netwok-simplex-algorithm"
+    "sec:important-mentions"
+    "sec:solution"
+    "subsec:dijkstra"
+    "subsec:edmonds-karp"
+    "sec:conclusion")
    (LaTeX-add-bibliographies
     "sample"))
  :latex)
